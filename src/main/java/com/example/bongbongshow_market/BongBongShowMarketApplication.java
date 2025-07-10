df962a0f4723a22571c1097889215289c46695f1
이용표 <166705762+boxinthechaos@users.noreply.github.com>
2025-07-10 15:31:21 +0900

    주가 변동률 수동으로 출력 (#1)
    * 주가 변동률 수동으로 출력
    
    * 15분 주기로 주가 변동률 띄우기 + html에 변동값 띄우기

---
+++ src/main/java/com/example/bongbongshow_market/BongBongShowMarketApplication.java
@@ -0,0 +1,14 @@
+package com.example.bongbongshow_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class BongBongShowMarketApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BongBongShowMarketApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/bongbongshow_market/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.example.bongbongshow_market.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
