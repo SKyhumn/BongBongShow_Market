33f4c03a76277209ef252f8eecf7ab7d25d9f523
ChoiJunYoung <s24052@gsm.hs.kr>
2025-07-15 20:43:54 +0900

    물가변동시세 페이지 완성

--- IDEAFESTIVAL/src/App.jsx
+++ IDEAFESTIVAL/src/App.jsx
@@ -3,25 +3,27 @@ import Home from './page/Home';
 import PriceChanging from './page/PriceChanging.jsx';
 import StockedItem from './page/StockedItem.jsx';
 import Ranking from './page/Ranking.jsx';
-import { Routes,Route,Link } from 'react-router-dom';
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <div className="Top">
-        <Link to='/' className="LINK"><h1 className="TITLE">봉봉쇼마켓</h1></Link>
-        <div className="Nav">
-          <Link to='/price-changing' className="btn LINK">물가 변동 상세</Link>
-          <Link to='/stocked-item' className="btn LINK">재고 물품</Link>
-          <Link to='/ranking' className="btn LINK">랭킹</Link>
+      <BrowserRouter>
+        <div className="Top">
+          <Link to='/' className="LINK"><h1 className="TITLE">봉봉쇼마켓</h1></Link>
+          <div className="Nav">
+            <Link to='/price-changing' className="btn LINK">물가 변동 상세</Link>
+            <Link to='/stocked-item' className="btn LINK">재고 물품</Link>
+            <Link to='/ranking' className="btn LINK">랭킹</Link>
+          </div>
         </div>
-      </div>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/price-changing' element={<PriceChanging/>}></Route>
-        <Route path='/stocked-item' element={<StockedItem/>}></Route>
-        <Route path='/ranking' element={<Ranking/>}></Route>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/price-changing' element={<PriceChanging/>}></Route>
+          <Route path='/stocked-item' element={<StockedItem/>}></Route>
+          <Route path='/ranking' element={<Ranking/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- IDEAFESTIVAL/src/page/PriceChanging.jsx
+++ IDEAFESTIVAL/src/page/PriceChanging.jsx
@@ -1,7 +1,38 @@
+import priceChange from '../dummy_data/priceChange.json';
+import './design/PriceChanging.css';
+import { useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer } from 'recharts';
+
 export default function PriceChanging(){
+    const [select, setSelected]=useState(null);
+    const [data, setData]=useState([]);
+    const clicked=(index)=>{
+        setSelected(index);
+        const chartData=priceChange.example[index]["price-change"];
+        setData(chartData);
+    };
     return(
-        <div>
-            
+        <div className="section">
+            <div className="selection">
+                {priceChange.example.map((a,index)=>(
+                    <p key={index} onClick={()=>clicked(index)} style={{color:select===index?'rgb(57, 80, 255)':'black'}}>{a.name}</p>
+                ))}
+            </div>
+            {select!==null&&(            
+                <div className="chart">
+                    <h1><span className="item">{priceChange.example[select].name}</span>의 가격변동</h1>
+                    <h3 className="changeRate">가격 변동률:{(((data.at(-1).y-data.at(-2).y)/data.at(-2).y)*100).toFixed(2)}%</h3>
+                    <ResponsiveContainer width="100%" height={400} className="priceChart">
+                        <LineChart data={data}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="x" />
+                            <YAxis />
+                            <Tooltip />
+                            <Line type="linear" dataKey="y" stroke="#3a6cf4" strokeWidth={2} />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+            )}
         </div>
     );
 }
